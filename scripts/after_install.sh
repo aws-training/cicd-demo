6988e050f1654b32402d047e9d2ca582fd0760e2
aws-training <40118116+aws-training@users.noreply.github.com>
2025-07-23 13:24:17 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-#sed -i "s|was deployed|was deployed on $escaped_instance_id in $escaped_az|g" /var/www/html/index.html
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+EC2_INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+EC2_AZ=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
+sed -i "s/was deployed/was deployed on $escaped_instance_id in $escaped_az/g" /var/www/html/index.html
